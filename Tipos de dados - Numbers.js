1e1919dc659532fe499adf403b0578338c64909f
walterdyna <alexdyna@gmail.com>
2023-04-07 12:39:19 -0300

    Aprendendo Numbers - no JS

---
+++ Tipos de dados - Numbers.js
@@ -0,0 +1,27 @@
+//NUMBERS
+
+
+const numberZero = 29       //numero --- pode ser usado como valor fixo 
+
+const numbertwo = "234/2"   //texto ---- e um texto e sera impresso como escrito
+
+const number3 = 234/2     //numero --- fazendo o peraçoes com numeros
+const number4 = 234/2+4-(2*5)
+
+const number5 = 30
+const number6 = 20
+
+//Some da variaveis
+const resultado = number5 + number6
+
+console.log(numberZero)
+
+console.log(numbertwo)
+
+console.log(number3)
+
+console.log(number4)
+
+console.log(resultado)
+
+console.log(number5 + number6)
